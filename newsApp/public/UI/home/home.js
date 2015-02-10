f98ff3b3208ac14142533df182b26538153e11a7
AlbyJoseph <alby.joseph@experionglobal.com>
2015-02-10 20:09:08 +0530

    first commit

--- newsApp/public/UI/home/home.js
+++ newsApp/public/UI/home/home.js
@@ -1,11 +1,10 @@
 angular.module('UI').controller('HomeCtrl',function($scope,$firebase){
 
-	var ref = new window.Firebase("https://radiant-inferno-9864.firebaseio.com/news");  
+	var ref = new window.Firebase("https://radiant-inferno-9864.firebaseio.com/news");
 	var fb = $firebase(ref);
 	var syncObject = fb.$asObject();
 	syncObject.$bindTo($scope, 'newsData');
-
-		$scope.add	=	function(item){
+			$scope.add	=	function(item){
 			var title	=	item.title;
 			var description	=	item.description;
 			var voteCount	=	0;
@@ -20,10 +19,56 @@ angular.module('UI').controller('HomeCtrl',function($scope,$firebase){
 		$scope.title="";
 		$scope.description="";
 	};
-	$scope.up	=	function(newItem){
+	$scope.up	=	function(newItem,newsData){
 		newItem.voteCount++;
+		var arrObj = [];
+                               var i = 0 ;
+                               for(var key in newsData){
+                                       arrObj[i]  = newsData[key];
+                                       i++;                                
+                               }
+                       console.log(arrObj);
+                       for(i = 2 ;i<arrObj.length;i++){
+                               for(var j = 2;j<arrObj.length - 1;j++){
+                                       if(arrObj[j].voteCount < arrObj[j+1].voteCount){
+                                               var t = arrObj[j];
+                                               arrObj[j] = arrObj[j+1];
+                                               arrObj[j+1] = t;
+                                       }
+                               }
+                       }
+                       i = 0 ;
+                       console.log(arrObj);
+                       for(key in newsData){
+                               newsData[key]  = arrObj[i];
+                               i++;                                
+                       }
+
 	};
-	$scope.down	=	function(newItem){
+	$scope.down	=	function(newItem,newsData){
 		newItem.voteCount--;
+		var arrObj = [];
+                               var i = 0 ;
+                               for(var key in newsData){
+                                       arrObj[i]  = newsData[key];
+                                       i++;                                
+                               }
+                       console.log(arrObj);
+                       for(i = 2 ;i<arrObj.length;i++){
+                               for(var j = 2;j<arrObj.length - 1;j++){
+                                       if(arrObj[j].voteCount < arrObj[j+1].voteCount){
+                                               var t = arrObj[j];
+                                               arrObj[j] = arrObj[j+1];
+                                               arrObj[j+1] = t;
+                                       }
+                               }
+                       }
+                       i = 0 ;
+                       console.log(arrObj);
+                       for(key in newsData){
+                               newsData[key]  = arrObj[i];
+                               i++;                                
+                       }
+
 	};
 });
